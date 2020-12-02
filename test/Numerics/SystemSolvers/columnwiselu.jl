1acd779b25ff77ab3d492a7763c00b7bb527ed3c
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2020-12-02 11:54:43 -0800

    Test LU solver for N = 0

--- test/Numerics/SystemSolvers/columnwiselu.jl
+++ test/Numerics/SystemSolvers/columnwiselu.jl
@@ -10,7 +10,8 @@ using ClimateMachine.SystemSolvers:
     band_lu_kernel!,
     band_forward_kernel!,
     band_back_kernel!,
-    DGColumnBandedMatrix
+    DGColumnBandedMatrix,
+    lower_bandwidth
 
 import ClimateMachine.MPIStateArrays: array_device
 
@@ -29,86 +30,90 @@ function band_to_full(B, p, q)
 end
 
 let
-    N = 1
-    Nq = N + 1
-    nstate = 3
-    nvertelem = 5
-    nhorzelem = 4
-    eband = 2
 
-    FT = Float64
-    m = n = Nq * nstate * nvertelem
-    p = q = Nq * nstate * eband - 1
+    @testset "$(@__FILE__) Columnwise LU" begin
+        for N in (0, 1)
+            Nq = N + 1
+            nstate = 3
+            nvertelem = 5
+            nhorzelem = 4
+            eband = 2
 
-    Random.seed!(1234)
-    AB = rand(FT, Nq, Nq, p + q + 1, n, nhorzelem)
+            FT = Float64
+            m = n = Nq * nstate * nvertelem
+            p = q = lower_bandwidth(N, nstate, eband)
 
-    AB[:, :, q + 1, :, :] .+= 10 # Make A's diagonally dominate
+            Random.seed!(1234)
+            AB = rand(FT, Nq, Nq, p + q + 1, n, nhorzelem)
 
-    Random.seed!(5678)
-    b = rand(FT, Nq, Nq, Nq, nstate, nvertelem, nhorzelem)
-    x = similar(b)
+            AB[:, :, q + 1, :, :] .+= 10 # Make A's diagonally dominate
 
-    perm = (4, 3, 5, 1, 2, 6)
-    bp = reshape(PermutedDimsArray(b, perm), n, Nq, Nq, nhorzelem)
-    xp = reshape(PermutedDimsArray(x, perm), n, Nq, Nq, nhorzelem)
+            Random.seed!(5678)
+            b = rand(FT, Nq, Nq, Nq, nstate, nvertelem, nhorzelem)
+            x = similar(b)
 
-    d_F = ArrayType(AB)
-    d_F = DGColumnBandedMatrix{
-        3,
-        N,
-        nstate,
-        nhorzelem,
-        nvertelem,
-        eband,
-        false,
-        typeof(d_F),
-    }(
-        d_F,
-    )
+            perm = (4, 3, 5, 1, 2, 6)
+            bp = reshape(PermutedDimsArray(b, perm), n, Nq, Nq, nhorzelem)
+            xp = reshape(PermutedDimsArray(x, perm), n, Nq, Nq, nhorzelem)
 
-    groupsize = (Nq, Nq)
-    ndrange = (Nq, Nq, nhorzelem)
+            d_F = ArrayType(AB)
+            d_F = DGColumnBandedMatrix{
+                3,
+                N,
+                nstate,
+                nhorzelem,
+                nvertelem,
+                eband,
+                false,
+                typeof(d_F),
+            }(
+                d_F,
+            )
 
-    event = Event(array_device(d_F.data))
-    event = band_lu_kernel!(array_device(d_F.data), groupsize, ndrange)(
-        d_F,
-        dependencies = (event,),
-    )
-    wait(array_device(d_F.data), event)
+            groupsize = (Nq, Nq)
+            ndrange = (Nq, Nq, nhorzelem)
 
-    F = Array(d_F.data)
+            event = Event(array_device(d_F.data))
+            event = band_lu_kernel!(array_device(d_F.data), groupsize, ndrange)(
+                d_F,
+                dependencies = (event,),
+            )
+            wait(array_device(d_F.data), event)
 
-    for h in 1:nhorzelem, j in 1:Nq, i in 1:Nq
-        B = AB[i, j, :, :, h]
-        G = band_to_full(B, p, q)
-        GLU = lu!(G, Val(false))
+            F = Array(d_F.data)
 
-        H = band_to_full(F[i, j, :, :, h], p, q)
+            for h in 1:nhorzelem, j in 1:Nq, i in 1:Nq
+                B = AB[i, j, :, :, h]
+                G = band_to_full(B, p, q)
+                GLU = lu!(G, Val(false))
 
-        @test H ≈ G
+                H = band_to_full(F[i, j, :, :, h], p, q)
 
-        xp[:, i, j, h] .= GLU \ bp[:, i, j, h]
+                @test H ≈ G
+
+                xp[:, i, j, h] .= GLU \ bp[:, i, j, h]
+            end
+
+            b = reshape(b, Nq * Nq * Nq, nstate, nvertelem * nhorzelem)
+            x = reshape(x, Nq * Nq * Nq, nstate, nvertelem * nhorzelem)
+
+            d_x = ArrayType(b)
+
+            event = Event(array_device(d_x))
+            event = band_forward_kernel!(array_device(d_x), groupsize, ndrange)(
+                d_x,
+                d_F,
+                dependencies = (event,),
+            )
+
+            event = band_back_kernel!(array_device(d_x), groupsize, ndrange)(
+                d_x,
+                d_F,
+                dependencies = (event,),
+            )
+            wait(array_device(d_x), event)
+
+            @test x ≈ Array(d_x)
+        end
     end
-
-    b = reshape(b, Nq * Nq * Nq, nstate, nvertelem * nhorzelem)
-    x = reshape(x, Nq * Nq * Nq, nstate, nvertelem * nhorzelem)
-
-    d_x = ArrayType(b)
-
-    event = Event(array_device(d_x))
-    event = band_forward_kernel!(array_device(d_x), groupsize, ndrange)(
-        d_x,
-        d_F,
-        dependencies = (event,),
-    )
-
-    event = band_back_kernel!(array_device(d_x), groupsize, ndrange)(
-        d_x,
-        d_F,
-        dependencies = (event,),
-    )
-    wait(array_device(d_x), event)
-
-    @test x ≈ Array(d_x)
 end
