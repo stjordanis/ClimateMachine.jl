ec37a01eb5e1d20ca5c1fa2a7036259264c8c721
Valeria Barra <valeriabarra21@gmail.com>
2021-01-15 16:37:02 -0800

    Use try-catch for negative examples
    Co-authored-by: Thomas H. Gibson <gibsonthomas1120@hotmail.com>

--- tutorials/Numerics/TimeStepping/explicit_lsrk.jl
+++ tutorials/Numerics/TimeStepping/explicit_lsrk.jl
@@ -10,9 +10,9 @@ FT = Float64
 
 # After discretizing in space, the semi-discretization of
 # the governing equations have the form:
-#
+# ``
 #  \frac{∂ Q}{∂ t} + ∇ ⋅ (F_1(Q)) == S(Q) [eq:governing]
-#
+# ``
 # [Intro something along these lines]
 
 # A single step of an ``s``-stage Runge-Kutta (RK) method for
@@ -38,26 +38,32 @@ FT = Float64
 ode_solver = ClimateMachine.ExplicitSolverType(
     solver_method = LSRK54CarpenterKennedy,
 )
-# CFL = FT(0.6)
+CFL = FT(0.4)
 timeend = FT(100)
-# run_simulation(ode_solver, CFL, timeend)
-
-# Here, we use a 4-th order 14-stage explict LSRK method:
-CFL = FT(1.7)
 run_simulation(ode_solver, CFL, timeend)
 
-# Oh-no it breaks!
-# Now we try using a 14-stage method. Due to its larger
-# stability region, we can take a larger time-step size
-# compared to the previous 5-stage method:
+# [Show example of the simulation running using the 5-stage LSRK
+# method.] Now let's try to increase the time-step size:
+try
+    CFL = FT(0.6)
+    run_simulation(ode_solver, CFL, timeend)
+catch err
+    @assert isa(err, DomainError)
+    println("The simulation blew up!")
+end
+
+# Uh oh, looks like the time integrator has gone unstable.
+# This is a constant issue when using a fully explicit method.
+# However, some explicit methods have larger stability regions
+# than others. For example, let's try using a 14-stage method:
 ode_solver = ClimateMachine.ExplicitSolverType(
     solver_method = LSRK144NiegemannDiehlBusch,
 )
 run_simulation(ode_solver, CFL, timeend)
 
 # ## [Strong-stability-preserving Runge-Kutta methods](@id ssprk)
-ode_solver = ClimateMachine.ExplicitSolverType(
-    solver_method = SSPRK33ShuOsher,
-)
-CFL = FT(1.7)
-run_simulation(ode_solver, CFL, timeend)
+# ode_solver = ClimateMachine.ExplicitSolverType(
+#     solver_method = SSPRK33ShuOsher,
+# )
+# CFL = FT(1.7)
+# run_simulation(ode_solver, CFL, timeend)
